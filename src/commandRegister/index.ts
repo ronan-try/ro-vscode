b78bbc9723ce8028434bf133af9846549e6cb303
Ronan <liruonan@hotmail.com>
2021-05-10 21:15:12 +0800

    update: vscode icons webview success

--- src/commandRegister/index.ts
+++ src/commandRegister/index.ts
@@ -2,9 +2,10 @@ import { ExtensionContext } from 'vscode';
 
 import HelloWorld from './helloWorld';
 import Mr from './mr';
+import Ui from './ui';
 
 export default function factory (context: ExtensionContext) {
   context.subscriptions.push(HelloWorld);
   context.subscriptions.push(Mr);
+  context.subscriptions.push(Ui(context));
 };
-
