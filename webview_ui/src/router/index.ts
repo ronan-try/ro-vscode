857082d16790cf09f3a8d732142ed80395b781ba
Ronan <liruonan@hotmail.com>
2021-05-10 21:15:36 +0800

    fix: imort.meta.env. 2020语法

--- webview_ui/src/router/index.ts
+++ webview_ui/src/router/index.ts
@@ -33,8 +33,8 @@ export const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   // history: createWebHistory(),
   routes,
 });
